ba31765375141ce40faf607373d282ffe75846fc
Timo von Holtz <tvh@tvholtz.de>
2017-07-09 19:51:15 +0200

    Model layout as SVG + Helpful tool

---
+++ output-model.py
@@ -0,0 +1,23 @@
+import argparse
+
+from keras.utils.visualize_util import plot
+from keras.models import load_model
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Visualize Model as Graph')
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Path to model h5 file. Model should be on the same path.'
+    )
+    parser.add_argument(
+        'image',
+        type=str,
+        help='Path to output image'
+    )
+    args = parser.parse_args()
+
+    model = load_model(args.model)
+    model.summary()
+
+    plot(model, to_file=args.image, show_shapes=True)
